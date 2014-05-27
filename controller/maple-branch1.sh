019f6ed49702f5122627d50b7d34aa1116567478
Andreas Voellmy <andreas.voellmy@gmail.com>
2014-05-27 00:46:24 -0400

    Update stats sampling period.

--- controller/maple-branch1.sh
+++ controller/maple-branch1.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-maple -u /vagrant/classes WANSelector -l 6633 --webserverportnum=8000  --dbfile=maple1.db
+maple -u /vagrant/classes WANSelector -l 6633 --webserverportnum=8000  --dbfile=maple1.db --flowstatssampleperiod=1000
